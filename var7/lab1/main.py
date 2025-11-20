91e268f202701dbea31e0ae75987c0f4b52f86fd
tokyoupscale <tokyoupscale@gmail.com>
2025-11-20 09:47:50 +0300

    fix lab1

--- var7/lab1/main.py
+++ var7/lab1/main.py
@@ -1,27 +1,49 @@
 import math
+from typing import List
+import numpy as np
 
-def f(x) -> float | 0:
+def f(x) -> float:
     d1 = math.sqrt((1/5) + (math.exp(x))**(1/5))
     d2 = abs(math.log(x**2) - 1.3)
     
     if d2 == 0:
         return 0
 
-    return d1/d2
+    result = d1/d2
+    return result
+
+def forsum(start: float, end: float, step: float) -> List[float]:
+    result = []
+    for x in np.arange(start, end, step):
+        sum = f(x)
+        result.append(sum)
+    return result
+
+def whilesum(start: float, end: float, step: float) -> List[float]:
+    result = []
+    x = start
+    while x <= end:
+        sum = f(x)
+        result.append(sum)
+        x += step
+    return result
 
 class Circle:
     # радиус - единица чего либо
     def __init__(self, radius):
         self.radius = radius
 
-    def area(self) -> float | 0:
+    def area(self) -> float:
         return math.pi * self.radius**2
     
-    def length(self) -> float | 0:
+    def length(self) -> float:
         return 2 * math.pi * self.radius
     
 def main():
     print(f(x=0.5))
+    print(forsum(0.1, 0.6, 0.1))
+    print(whilesum(0.1, 0.6, 0.1))
+
     print(Circle(5).area())
     print(Circle(5).length())
 
