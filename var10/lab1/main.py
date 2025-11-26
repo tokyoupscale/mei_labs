bb4229eb5edbd644d66b3116f059920fbf2ddd77
tokyoupscale <tokyoupscale@gmail.com>
2025-11-26 16:45:26 +0300

    fix lab1v10

--- var10/lab1/main.py
+++ var10/lab1/main.py
@@ -1,10 +1,16 @@
 import math
-import numpy as np
 from typing import List
+import numpy as np
 
-def f(x) -> float | None:    
-    equation = 1.1*math.exp(x) + abs(math.cos(math.sqrt(math.pi*x))) - (4/9)
-    return equation
+def f(x) -> float:
+    d1 = math.sqrt((1/5) + (math.exp(x))**(1/5))
+    d2 = abs(math.log(x**2) - 1.3)
+    
+    if d2 == 0:
+        return 0
+
+    result = d1/d2
+    return result
 
 def forsum(start: float, end: float, step: float) -> List[float]:
     result = []
@@ -23,22 +29,34 @@ def whilesum(start: float, end: float, step: float) -> List[float]:
     return result
 
 class Dog:
-    # радиус - единица чего либо
-    def __init__(self, breed, age, name):
-        self.breed = breed
-        self.age = age
+    def __init__(self, name, age, kind):
         self.name = name
-    
-    def tell_about(self) -> str:
-        phrase = f"Кличка: {self.name}, возраст: {self.age}, порода: {self.breed}"
-        return phrase
+        self.age = age
+        self.kind = kind
 
+    def bark(self):
+        return print("Bark!")
+    
+    def whoami(self, name: str):
+        return print(f"эту собаку зовут {name}")
+    
+    def howold(self, age: int):
+        return print(f"этой собаке {age} лет")
+    
+    def whatkind(self, kind :str):
+        return print(f"эта собака породы {kind}")
+
+    
 def main():
     print(f(x=0.5))
     print(forsum(0.1, 0.6, 0.1))
     print(whilesum(0.1, 0.6, 0.1))
 
-    print(Dog("лабрадор", 5, 'челикс').tell_about())
+    dog = Dog("Рекс", 5, "шпиц")
+    dog.bark()
+    dog.whoami("Рекс")
+    dog.howold(5)
+    dog.whatkind("шпиц")
 
 if __name__ == "__main__":
     main()
